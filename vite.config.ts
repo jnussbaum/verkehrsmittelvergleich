ea469652e4a5cc3f541de76086b109f10e2e9d01
Johannes Nussbaum <johannes.nussbaum@dasch.swiss>
2025-02-10 21:19:46 +0100

    try configuring eslint

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgr from 'vite-plugin-svgr';
+import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(), 
+    eslint(),
     viteTsconfigPaths(),
     svgr({
       include: '**/*.svg?react',
